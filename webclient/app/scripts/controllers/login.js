1b6d57dde9e48bcb2afe4dc17f072c8aacae839b
nam <nvtnam@gmail.com>
2015-08-05 17:17:02 +0700

    ws

--- webclient/app/scripts/controllers/login.js
+++ webclient/app/scripts/controllers/login.js
@@ -38,7 +38,7 @@ angular.module('webclientApp')
       $scope.login = function() {
 
         var payload = {
-          email : this.email,
+          username : this.username,
           password : this.password
         };
 
@@ -46,7 +46,7 @@ angular.module('webclientApp')
             .error(function(data, status){
               if(status === 400) {
                 angular.forEach(data, function(value, key) {
-                  if(key === 'email' || key === 'password') {
+                  if(key === 'username' || key === 'password') {
                     alertService.add('danger', key + ' : ' + value);
                   } else {
                     alertService.add('danger', value.message);
---
+++ webclient/app/scripts/services/websocket.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name webclientApp.websocket
+ * @description
+ * # websocket
+ * Service in the webclientApp.
+ */
+angular.module('webclientApp')
+  .service('websocket', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
